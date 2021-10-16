4e4ee3f556cfcd414100192c01beb7e721afbd5e
Ashish <108119028@nitt.edu>
2021-10-17 00:02:12 +0530

    add folders

---
+++ intro-to-node/index.js
@@ -0,0 +1,8 @@
+var superheroes= require("superheroes");
+var supervillains=require("supervillains");
+
+var mysupervillainName=supervillains.random();
+var mysuperheroname=superheroes.random();
+
+console.log(mysuperheroname);
+console.log(mysupervillainName);
